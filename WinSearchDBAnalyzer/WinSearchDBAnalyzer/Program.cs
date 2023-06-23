c5b30961eaa0a7d9c1469fd3bb75e52cf57dcfe7
Andrew Rathbun <36825567+AndrewRathbun@users.noreply.github.com>
2023-06-23 16:10:58 -0400

    Update Program.cs

--- WinSearchDBAnalyzer/WinSearchDBAnalyzer/Program.cs
+++ WinSearchDBAnalyzer/WinSearchDBAnalyzer/Program.cs
@@ -1,61 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
 
 namespace WinSearchDBAnalyzer
 {
     static class Program
     {
         /// <summary>
-        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(ResolveAssembly);
+            AppDomain.CurrentDomain.AssemblyResolve += ResolveAssembly;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
-        // 리소스 dll 취득
 
-        private static System.Reflection.Assembly ResolveAssembly(object sender, ResolveEventArgs args)
+        private static Assembly ResolveAssembly(object sender, ResolveEventArgs args)
         {
-
-            System.Reflection.Assembly thisAssembly = System.Reflection.Assembly.GetExecutingAssembly();
-
+            Assembly thisAssembly = Assembly.GetExecutingAssembly();
             string name = args.Name.Substring(0, args.Name.IndexOf(',')) + ".dll";
-
             var files = thisAssembly.GetManifestResourceNames().Where(s => s.EndsWith(name));
 
-
-
-            if (files.Count() > 0)
+            if (files.Any())
             {
-
                 string fileName = files.First();
 
-                using (System.IO.Stream stream = thisAssembly.GetManifestResourceStream(fileName))
+                using (Stream stream = thisAssembly.GetManifestResourceStream(fileName))
                 {
-
                     if (stream != null)
                     {
-
-                        byte[] data = new byte[stream.Length];
-
-                        stream.Read(data, 0, data.Length);
-
-                        return System.Reflection.Assembly.Load(data);
-
+                        MemoryStream ms = new MemoryStream();
+                        stream.CopyTo(ms);
+                        byte[] data = ms.ToArray();
+                        return Assembly.Load(data);
                     }
-
                 }
-
             }
-
             return null;
-
         }
     }
 }
